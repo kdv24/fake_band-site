93fd1ab709e6dbacce0dd69256970cef6dbf960e
Kelly de Vries <kdv@me.com>
2015-02-20 09:09:12 -0800

    Add header file to inc.

---
+++ inc/header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A layout example that shows off a responsive pricing table.">
+
+    <title>Screaming Newspapers</title>
+
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+
+<!--[if lte IE 8]>
+  
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie-min.css">
+  
+<![endif]-->
+<!--[if gt IE 8]><!-->
+  
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+  
+<!--<![endif]-->
+
+  
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="css/layouts/pricing-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="css/layouts/pricing.css">
+    <!--<![endif]-->
+</head>
+
+<body>
+    <div class="pure-menu pure-menu-open pure-menu-horizontal">
+        <a href="#" class="pure-menu-heading">Screaming Newspapers</a>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li class="pure-menu-selected"><a href="#">Albums</a></li>
+            <li><a href="#">Tour</a></li>
+        </ul>
+    </div>
+
+    <div class="banner">
+        <h1 class="banner-head">
+            Screaming Newspapers<br>
+            Rockin' the World!</h1>
+    </div>
+</body>
+</html>    
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     $bandMember4 = $_GET["bandMember4"];    
     $bandMember5 = $_GET["bandMember5"];
     $bandMember6 = $_GET["bandMember6"];
+    $bandMembers = array($bandMember1, $bandMember2, $bandMember3, $bandMember4, $bandMember5, $bandMember6);
 ?>    
 
 <?php
@@ -14,6 +15,7 @@
     $album4 = $_GET["album4"];    
     $album5 = $_GET["album5"];
     $album6 = $_GET["album6"];
+    $albums = array($album1, $album2, $album3, $album4, $album5, $album6); 
 ?>   
 
 <?php
@@ -23,6 +25,7 @@
     $tourDate4 = $_GET["tourDate4"];    
     $tourDate5 = $_GET["tourDate5"];
     $tourDate6 = $_GET["tourDate6"];
+    $tourDates = array($tourDate1, $tourDate2, $tourDate3, $tourDate4, $tourDate5, $tourDate6);    
 ?> 
 
 
