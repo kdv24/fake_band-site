8c68a64298335368783ec37b4545aedce56ba964
Kelly de Vries <kdv@me.com>
2015-02-20 09:58:14 -0800

    Add inc/variables.php file to share between files.

---
+++ inc/variables.php
@@ -0,0 +1,29 @@
+<?php
+    $bandMember1 = $_GET["bandMember1"];
+    $bandMember2 = $_GET["bandMember2"];
+    $bandMember3 = $_GET["bandMember3"];
+    $bandMember4 = $_GET["bandMember4"];    
+    $bandMember5 = $_GET["bandMember5"];
+    $bandMember6 = $_GET["bandMember6"];
+    $bandMembers = array($bandMember1, $bandMember2, $bandMember3, $bandMember4, $bandMember5, $bandMember6);
+?>    
+
+<?php
+    $album1 = $_GET["album1"];
+    $album2 = $_GET["album2"];
+    $album3 = $_GET["album3"];
+    $album4 = $_GET["album4"];    
+    $album5 = $_GET["album5"];
+    $album6 = $_GET["album6"];
+    $albums = array($album1, $album2, $album3, $album4, $album5, $album6); 
+?>   
+
+<?php
+    $tourDate1 = $_GET["tourDate1"];
+    $tourDate2 = $_GET["tourDate2"];
+    $tourDate3 = $_GET["tourDate3"];
+    $tourDate4 = $_GET["tourDate4"];    
+    $tourDate5 = $_GET["tourDate5"];
+    $tourDate6 = $_GET["tourDate6"];
+    $tourDates = array($tourDate1, $tourDate2, $tourDate3, $tourDate4, $tourDate5, $tourDate6);    
+?> 
